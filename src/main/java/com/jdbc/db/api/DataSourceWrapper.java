8e4a2992a3b67f2494d247b580853a66dc29476d
Michalenok Marina <mihalenokee@outlook.com>
2023-07-11 13:58:08 +0300

    feat: clean

--- src/main/java/com/jdbc/db/api/DataSourceWrapper.java
+++
@@ -1,8 +0,0 @@
-package com.jdbc.db.api;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface DataSourceWrapper extends AutoCloseable{
-    Connection getConnection() throws SQLException;
-}
