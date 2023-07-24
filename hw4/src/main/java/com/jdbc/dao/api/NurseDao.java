0626e002ef7e818ee4d67d97b3d073ed5d7eed93
Michalenok Marina <mihalenokee@outlook.com>
2023-07-24 23:26:59 +0300

    feat: update structure

---
+++ hw4/src/main/java/com/jdbc/dao/api/NurseDao.java
@@ -0,0 +1,15 @@
+package com.jdbc.dao.api;
+
+import com.jdbc.entity.Nurse;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NurseDao extends Transactional{
+    Long create (Nurse t);
+    void update(Long id, Nurse t);
+    void delete(Long id);
+    List<Nurse> findAll();
+    Optional<Nurse> findNurseById(Long id);
+
+}
